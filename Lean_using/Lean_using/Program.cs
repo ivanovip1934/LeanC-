5f47e45041d262724e7a3808b79e3d394b686949
ivanovip1934 <ivanovip1934@outlook.com>
2019-03-28 23:17:51 +0300

    Создали пример использования потока чтения и записи файла с автоматическим удалением потоков.

---
+++ Lean_using/Lean_using/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Lean_using
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (StreamWriter sw = new StreamWriter(@"c:\programs\lean_using.txt")) {
+                sw.WriteLine("Как победить Капитана Великолепного");
+                sw.WriteLine("Еще один гениальный секретный план");
+                sw.WriteLine("от Жулика");
+            }
+            using (StreamReader sr = new StreamReader(@"c:\programs\lean_using.txt")) {
+                string line;
+                while (!sr.EndOfStream){
+                    line = sr.ReadLine();
+                    Console.WriteLine(line);
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
